6eb526f03913afcb4500923c06d84669948bf7c9
Kehvarl <ACorbett@SecureSourceUSA.com>
2017-10-11 14:29:19 -0700

    Raycasting fov algorithm

--- Map/fov_map.py
+++ Map/fov_map.py
@@ -8,10 +8,10 @@ class Line:
     http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm#Python
     """
     def __init__(self, x0, y0, x1, y1):
-        self.points = []
-        self.calculate_line(x0, y0, x1, y1)
+        self.points = self.get_points(x0, y0, x1, y1)
 
-    def calculate_line(self, x0, y0, x1, y1):
+    @staticmethod
+    def get_points(x0, y0, x1, y1):
         # Initial conditions
         dx = x1 - x0
         dy = y1 - y0
@@ -41,10 +41,10 @@ class Line:
 
         # Iterate over bounding box and generate points between start and end
         y = y1
-        self.points = []
+        points = []
         for x in range(x0, x1 + 1):
             coord = (y, x) if is_steep else (x, y)
-            self.points.append(coord)
+            points.append(coord)
             error -= abs(dy)
             if error < 0:
                 y += y_step
@@ -52,38 +52,40 @@ class Line:
 
         # Reverse the list if the start and end were swapped
         if swapped:
-            self.points.reverse()
+            points.reverse()
 
-
-class ViewTile:
-    def __init__(self, visible=False):
-        self.visible = visible
+        return points
 
 
 class FieldOfView:
     def __init__(self, game_map):
         self.game_map = game_map
-        self.visible = [
-            [ViewTile(False)
-             for _ in range(game_map.height)]
-            for _ in range(game_map.width)]
+        self.visible = self.initialize_view()
         self.tiles = [
             [Tile()
              for _ in range(game_map.height)]
             for _ in range(game_map.width)]
 
-    def update(self, player_x, player_y, sight_radius=10):
-        sight_range = sight_radius * sight_radius
+    def initialize_view(self):
+        visible = [
+            [False
+             for _ in range(self.game_map.height)]
+            for _ in range(self.game_map.width)]
+        return visible
 
+    def update(self, player_x, player_y, sight_radius=10):
+        self.visible = self.initialize_view()
+        sight_range = sight_radius * sight_radius
         for y in range(-sight_radius, sight_radius):
             for x in range(-sight_radius, sight_radius):
-                if x*x + y*y > sight_range:
+                if x * x + y * y > sight_range:
+                    continue
+                if not self.game_map.point_in_map(player_x + x, player_y + y):
                     continue
 
-                if not (0 < player_y + y < self.game_map.height and
-                        0 < player_x + x < self.game_map.width):
-                    continue
-
-                for point_x, point_y in Line(player_x, player_y, player_x+x, player_y + y).points:
-                    self.tiles[point_x][point_y] = self.game_map.tiles[point_x][point_y]
-                    self.visible[point_x][point_y].visible = True
+                points = Line.get_points(player_x, player_y, player_x + x, player_y + y)
+                print(points)
+                for point in points:
+                    if self.game_map.point_in_map(point[0], point[1]):
+                        self.tiles[point[0]][point[1]] = self.game_map.tiles[point[0]][point[1]]
+                        self.visible[point[0]][point[1]] = True
